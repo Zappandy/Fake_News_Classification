ab3ab688b7cb8385d8155e4f77e4347dc49c866f
Andres <nanoandres_24@hotmail.com>
2022-01-05 23:31:06 +0100

    messy py files, fixing in local atm

--- data_preprocessing.py
+++ data_preprocessing.py
@@ -1,6 +1,7 @@
 import pandas as pd
 import nltk
 import feather
+import collections
 from sklearn.model_selection import train_test_split
 from keras.preprocessing.text import text_to_word_sequence
 
@@ -26,9 +27,29 @@ df["text"] = df["text"].str.lower().apply(text_to_word_sequence)  # 23481 rows, 
 #X_train, X_test, y_train, y_set = train_test_split(df["text"], df["label"], test_size=0.33)
 X_train, X_test, y_train, y_set = train_test_split(df["text"], df["label"], train_size=0.8, test_size=0.2)
 X_train, X_valid, y_train, y_valid = train_test_split(df["text"], df["label"], train_size=0.9, test_size=0.1)
+#X_train.reset_index(inplace=True) do we need index? no, this is a series anyway
+# y_train.to_frame()  # does not have in place?
+articles = X_train.values.tolist()
+# number of articles containing one word from vocab. That is why it's a set
+frequencies = collections.Counter(word for art in articles for word in set(art))  # this is better
+vocabulary = frequencies.keys()
+#vocabulary = set(word for sent in articles for word in sent)  # takes 2 min
+word2index = {word: e for e, word in enumerate(vocabulary)}
+
+total_articles = X_train.shape[0]
+num_words = len(list(vocabulary)) #  needed, yes for TF
+# https://www.askpython.com/python/examples/tf-idf-model-from-scratch
+# https://towardsdatascience.com/tf-idf-for-document-ranking-from-scratch-in-python-on-real-world-dataset-796d339a4089
+# https://www.youtube.com/watch?v=vZAXpvHhQow
+# https://www.youtube.com/watch?v=lBO1L8pgR9s
+
+
 # bag of words does NOT uphold the order of the sentences
 # https://stackoverflow.com/questions/55492666/what-is-better-to-use-keras-preprocessing-tokenizer-or-nltk-tokenize  understanding tokenization
 #train, validate, test = np.split(df.sample(frac=1), [int(.6*len(df)), int(.8*len(df))])
 
 # why tf idf to solve issues with word count https://www.youtube.com/watch?v=76jmgV_ZPUs
 # tf idf better at longer documents than word embeddings
+# https://towardsdatascience.com/understanding-word-embeddings-with-tf-idf-and-glove-8acb63892032
+# tf-idf is a sparse vector representation instead of a dense one like GloVe. GloVe is closer to a Word2vec, i.e. a word embedding
+
--- decision_tree.py
+++ decision_tree.py
@@ -1,3 +1,4 @@
 # https://anderfernandez.com/en/blog/code-decision-tree-python-from-scratch/
 
 # Gin index --> cost function
+# https://anderfernandez.com/en/blog/code-decision-tree-python-from-scratch/
