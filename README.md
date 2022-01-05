f39c754917e69838a0a09ad8cf8a7674a5e3bfbf
Andres <nanoandres_24@hotmail.com>
2022-01-05 20:34:45 +0100

    changed tokenization and added links to the README

--- README.md
+++ README.md
@@ -7,6 +7,7 @@ decision tree
 - https://anderfernandez.com/en/blog/code-decision-tree-python-from-scratch/
 - http://www.odbms.org/wp-content/uploads/2014/07/DecisionTrees.pdf
 - https://towardsdatascience.com/classification-decision-trees-easily-explained-f1064dde175e
+- https://www.researchgate.net/publication/350287290_Fake_News_Classification_Using_Random_Forest_and_Decision_Tree_J48
 
 splitting data
 - https://stackabuse.com/scikit-learns-traintestsplit-training-testing-and-validation-sets/
--- data_preprocessing.py
+++ data_preprocessing.py
@@ -2,6 +2,9 @@ import pandas as pd
 import nltk
 import feather
 from sklearn.model_selection import train_test_split
+from keras.preprocessing.text import text_to_word_sequence
+
+# prioritizing keras over nltk for preprocessing
 #nltk.download('punkt') only download once
 from nltk.tokenize import word_tokenize # if possible, otherwise .split()
 
@@ -13,19 +16,19 @@ from nltk.tokenize import word_tokenize # if possible, otherwise .split()
 
 fake = pd.read_feather("Fake_News.feather")
 true = pd.read_feather("Factual_News.feather")
-print(fake)
 
-#true["label"] = 0 
-#fake["label"] = 1
-#
-#df = pd.concat([true, fake]).sample(frac=1) # shuffle rows to create a representative train/test set
-#df["text"] = df["text"].str.lower().apply(word_tokenize)
-#print(df.head())
-#print(df.shape)
-##X_train, X_test, y_train, y_set = train_test_split(df["text"], df["label"], test_size=0.33)
-#X_train, X_test, y_train, y_set = train_test_split(df["text"], df["label"], train_size=0.8, test_size=0.2)
-#X_train, X_valid, y_train, y_valid = train_test_split(df["text"], df["label"], train_size=0.9, test_size=0.1)
-##print(type(X_train))
-#print(X_train.head())
+true["label"] = 0 
+fake["label"] = 1
 
+df = pd.concat([true, fake]).sample(frac=1) # shuffle rows to create a representative train/test set
+# since they are articles, maybe sent tokenize is better
+df["text"] = df["text"].str.lower().apply(text_to_word_sequence)  # 23481 rows, takes less than a min
+#X_train, X_test, y_train, y_set = train_test_split(df["text"], df["label"], test_size=0.33)
+X_train, X_test, y_train, y_set = train_test_split(df["text"], df["label"], train_size=0.8, test_size=0.2)
+X_train, X_valid, y_train, y_valid = train_test_split(df["text"], df["label"], train_size=0.9, test_size=0.1)
+# bag of words does NOT uphold the order of the sentences
+# https://stackoverflow.com/questions/55492666/what-is-better-to-use-keras-preprocessing-tokenizer-or-nltk-tokenize  understanding tokenization
 #train, validate, test = np.split(df.sample(frac=1), [int(.6*len(df)), int(.8*len(df))])
+
+# why tf idf to solve issues with word count https://www.youtube.com/watch?v=76jmgV_ZPUs
+# tf idf better at longer documents than word embeddings
