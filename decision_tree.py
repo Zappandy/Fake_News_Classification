cf6b531690f3586daa7e846408dce6196391d73e
Andres <nanoandres_24@hotmail.com>
2022-01-08 20:46:18 +0100

    trying to fix this py and feather mess

--- decision_tree.py
+++
@@ -1,4 +0,0 @@
-# https://anderfernandez.com/en/blog/code-decision-tree-python-from-scratch/
-
-# Gin index --> cost function
-# https://anderfernandez.com/en/blog/code-decision-tree-python-from-scratch/
