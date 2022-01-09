54500bc8625ce2aeb906203c00128cc28a4a3a8a
Andres <nanoandres_24@hotmail.com>
2022-01-09 22:48:04 +0100

    writing decision tree

--- data_preprocessing.py
+++ data_preprocessing.py
@@ -1,25 +1,25 @@
 import numpy as np
 import pandas as pd
-import nltk
-import feather
 import collections
-from sklearn.model_selection import train_test_split
-from keras.preprocessing.text import text_to_word_sequence
 
-# prioritizing keras over nltk for preprocessing
-#nltk.download('punkt') only download once
-from nltk.tokenize import word_tokenize # if possible, otherwise .split()
-
+X_train = pd.read_feather("X_train.feather")
+X_test = pd.read_feather("X_test.feather")
+X_valid = pd.read_feather("X_valid.feather")
+y_train = pd.read_feather("y_train.feather")
+y_test = pd.read_feather("y_test.feather")
+y_valid = pd.read_feather("y_valid.feather")
 articles = X_train.values.tolist()
 # number of articles containing one word from vocab. That is why it's a set
-frequencies = collections.Counter(word for art in articles for word in set(art))  # this is better
+frequencies = collections.Counter(word for art in articles for word in set(art[0]))  # this is better
 vocabulary = frequencies.keys()
 word2index = {word: e for e, word in enumerate(vocabulary)}
 
 total_articles = X_train.shape[0]
 
+
 def TF(art, word):  # term_frequency
-    return len([w for w in art if w == word])/len(art)
+    #return len([w for w in art if w == word])/len(art)  # takes too long
+    return len([w for w in art if np.any(w[0] == word)])/len(art)  # takes too long
 
 def IDF(word, frequencies, total_articles):  # inverse_document_freq
     if frequencies[word]:
@@ -30,13 +30,15 @@ def IDF(word, frequencies, total_articles):  # inverse_document_freq
 
 def TF_IDF(article):
     tf_idf_vec = np.zeros((len(vocabulary),))
-    for word in article:
+    for word in article[0]:
         tf_idf_vec[word2index[word]] = TF(article, word) * IDF(word, frequencies, total_articles)
     return tf_idf_vec
 
-tf_idf_vectors = [tf_idf_vectors(art) for art in articles]
 
-print(tf_idf_vectors[0])
+tf_idf_vectors = [TF_IDF(art) for art in articles]
+#stored_vectors = pd.DataFrame(np.concatenate(tf_idf_vectors))  # dangerous 4 this task
+stored_vectors = pd.DataFrame(np.row_stack(tf_idf_vectors))
+
 
 # https://www.askpython.com/python/examples/tf-idf-model-from-scratch
 # https://towardsdatascience.com/tf-idf-for-document-ranking-from-scratch-in-python-on-real-world-dataset-796d339a4089
@@ -53,3 +55,4 @@ print(tf_idf_vectors[0])
 # https://towardsdatascience.com/understanding-word-embeddings-with-tf-idf-and-glove-8acb63892032
 # tf-idf is a sparse vector representation instead of a dense one like GloVe. GloVe is closer to a Word2vec, i.e. a word embedding
 
+# concat vs stack https://deeplizard.com/learn/video/kF2AlpykJGY
--- decision_tree.py
+++ decision_tree.py
@@ -1,4 +1,52 @@
-# https://anderfernandez.com/en/blog/code-decision-tree-python-from-scratch/
+import pandas as pd
+import numpy as np
 
-# Gin index --> cost function
+data = pd.read_csv("data.csv")
+#data = pd.read_csv("vectors.csv", skipfooter=2000000, engine="python"))
+data["obese"] = (data.Index >= 4).astype("int")
+data.drop("Index", axis=1, inplace = True)
+print(data.head())
+
+
+# cost functions gin index  vs entropy 
+
+def gini_index(x):
+    """
+    loss function: 1 - sigma(prob_data)**2
+    """
+    if isinstance(x, pd.Series):
+        x_prob = x.value_counts() / x.shape[0]  # vector size
+        #entropy = np.sum(-a*np.log2(a+1e-9))  
+    return 1 - np.sum(x_prob**2)
+
+def entropy(x):
+    """
+    loss function: sigma(-x_prob * log2(x_prob))
+    """
+    if isinstance(x, pd.Series):
+        x_prob = x.value_counts() / x.shape[0]  # vector size
+        return np.sum(-x_prob*np.log2(x_prob))  # entropy 
+        #return np.sum(-x_prob*np.log2(x_prob+1e-9))  # entropy 
+# information gain for classification
+
+#https://www.section.io/engineering-education/entropy-information-gain-machine-learning/
+
+# y --> target
+
+#https://www.youtube.com/watch?v=sgQAhG5Q7iY
+def information_gain_classification(y, mask, func):  # information gain better with entropy than gini
+
+    """
+    information gain of a loss function
+    y --> target
+    mask --> split choice
+    """
+    a = sum(mask)
+    b = 
+    entropy(total_set) - np.sum(x/total_set * entropy(x))
+
+# entropy --> https://www.youtube.com/watch?v=y6VwIcZAUkI
+
+
 # https://anderfernandez.com/en/blog/code-decision-tree-python-from-scratch/
+# good comments on how DT work under the drop Index cell
