8f807319e4f6c0c394aadb7c1d60c50392acf2b4
Andres <nanoandres_24@hotmail.com>
2022-01-10 22:14:24 +0100

    removed empty lines

--- data_preprocessing.py
+++ data_preprocessing.py
@@ -5,11 +5,10 @@ from keras.preprocessing.text import text_to_word_sequence
 
 articles = pd.read_csv("all_datasets.csv")
 articles["text"] = articles["text"].str.lower().apply(text_to_word_sequence)
-total_articles = articles.shape[0]
+total_articles = articles.shape[0]  # 44898
 max_len = max(articles["text"].str.len())  # len method works with list
 
 # number of articles containing one word from vocab. That is why it's a set
-
 frequencies = collections.Counter(word for art in articles["text"] for word in set(art))  # this is better
 vocabulary = frequencies.keys()  # 138021, this would be for sentences? nah per article
 word2index = {word: e for e, word in enumerate(vocabulary)}
@@ -36,8 +35,5 @@ class TF_IDF:
             tf_idf_vec[word2index[word]] = self.TF(article, word) * self.IDF(word)
         return tf_idf_vec
     
-
-
 compute_tf_idf = TF_IDF()
 example = compute_tf_idf.article_tf_idf(articles["text"].iloc[0,]) 
-
